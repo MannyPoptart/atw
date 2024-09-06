0ae45ea6271071d3104465c8a8e263e7bc1dd0b2
Manny Aguilera <maguilera5@ewu.edu>
2024-09-06 06:28:10 +0000

    themes

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -1,7 +1,9 @@
 let theme = 'light';
 
-function changeTheme() {
-	onclick = () => {
-		theme === 'light' ? theme = 'dark' : theme = 'light';
+export function changeTheme(theme) {
+	if (typeof document != 'undefined') {
+	  // Ensure this runs only on the client
+	  document.documentElement.setAttribute('data-theme', theme);
 	}
-}
+  }
+  
