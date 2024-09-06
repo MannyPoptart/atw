6ca7a67cf2412f1aaebbbd2de38018f2e3da0127
Manny Aguilera <maguilera5@ewu.edu>
2024-09-06 07:07:29 +0000

    theme changer added

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -1,9 +1,17 @@
+
 let theme = 'light';
+const savedTheme = localStorage.getItem('theme') || 'coffee';
+document.documentElement.setAttribute('data-theme', savedTheme);
 
 export function changeTheme(theme) {
 	if (typeof document != 'undefined') {
 	  // Ensure this runs only on the client
 	  document.documentElement.setAttribute('data-theme', theme);
+	  localStorage.setItem('theme', theme);
+	  console.log('Theme changed to', theme);
 	}
   }
+  window.changeTheme = changeTheme;
+
+  
   
